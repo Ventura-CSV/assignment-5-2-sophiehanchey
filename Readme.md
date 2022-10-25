1aece917a45bc2daf440f4e7f59cf691de0e60ab
Kyuwoong Lee <kwooler@gmail.com>
2022-10-24 22:35:08 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+[Link to Chap 5 Assignment 1](https://docs.google.com/presentation/d/1r3h2R9JwK9HK_U2Ia-zncL0BSjHV6Giu6ugNJ6yZpgc/edit#slide=id.g1219ddca117_0_6)
+
+![A01](https://nimbus-screenshots.s3.amazonaws.com/s/9528d8bafeb76b1a69607213a629242c.png)
+
+## Compete the "main.py"
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+
+def sumoProduct(l1, l2):
+    ##################################################
+    # make your code
+    ##################################################
+    return sumP
+
+
+numbers1 = [5, 3, 1, 1, 2]
+numbers2 = [1, 2, 2, 9, 5]
+result = sumoProduct(numbers1, numbers2)
+print('Your result : ', result)
+
+
+numbers1 = [random.randint(1, 10) for i in range(5)]
+numbers2 = [random.randint(1, 10) for i in range(5)]
+print('numbers 1 ', numbers1)
+print('numbers 2 ', numbers2)
+result = sumoProduct(numbers1, numbers2)
+print('Your result : ', result)
