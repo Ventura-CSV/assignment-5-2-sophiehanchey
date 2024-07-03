4f047f0b3cf610851afc86c19a121e7ae3347778
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 23:05:42 -0700

    used if statement to fix error

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def isPrime(num):
+    # check value isn't zero or one
+    if num == 0 or num == 1:
+        return False
     for i in range(2, num):
         if num % i == 0:
             return False
